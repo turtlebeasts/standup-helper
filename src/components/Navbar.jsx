ab8f2494990fb8543f90bb03a3c109653507d098
turtlebeasts <179turtleshells@gmail.com>
2025-07-25 08:06:36 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,11 @@ const Navbar = () => {
   return (
     <nav className="w-full px-6 py-4 flex justify-between items-center shadow-md bg-white dark:bg-gray-900 dark:text-white">
       <Link to="/" className="text-xl font-bold tracking-wide">
-        JokeDeck
+        Standup-Helper
       </Link>
 
       <div className="space-x-4">
         <SlideInModal />
-        <span className="ml-4">🌓</span>
       </div>
     </nav>
   );
